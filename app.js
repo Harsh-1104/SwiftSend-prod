2d8e4281b5118746686500b0ec911b519b03db3e
Gajjar Harsh <gajjarharah1104@gmail.com>
2024-02-22 11:49:36 +0530

    Small changes

--- app.js
+++ app.js
@@ -85,17 +85,17 @@ let instance = new Razorpay({
 app.use(express.json());
 app.use(fileUpload());
 app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(['/docs/assets', '/instance/assets', '/assets'], express.static("assets"));
 app.use("/", router);
 app.use(cors());
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+// });
 
 app.use(sessions({
     resave: false,
